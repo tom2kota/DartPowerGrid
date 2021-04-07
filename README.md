5f03db6360953ce9f44a31a29c9484403e983e4e
Web Dev <tom2kota@gmail.com>
2021-04-07 15:12:07 +0300

    Add basic implementation of using classes in Dart

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ Samples of code refactoring of Dart project
 
 
 Copy & paste code to: [dartpad.dev](https://dartpad.dev)
+
+
+
+-----
+
+
+* [List<E> class](https://api.dart.dev/stable/2.12.2/dart-core/List-class.html)
--- src/power_supply_01.dart
+++ src/power_supply_01.dart
@@ -1,11 +1,36 @@
-void main(){
+void main() {
+  PowerGrid grid = new PowerGrid();
+  NuclearPlant nuclear = new NuclearPlant();
+  SolarPlant solar = new SolarPlant();
 
+  grid.addPlant(nuclear);
+  grid.addPlant(solar);
 }
 
 class PowerGrid {
+  // List<NuclearPlant> connectedPlants = [];
+  //
+  // addPlant(NuclearPlant plant) {
+  //   plant.turnOn();
+  //   connectedPlants.add(plant);
+  // }
 
+  List<dynamic> connectedPlants = [];
+
+  addPlant(dynamic plant) {
+    plant.turnOn();
+    connectedPlants.add(plant);
+  }
 }
 
-class NuclearPlant{
+class NuclearPlant {
+  turnOn() {
+    print('Nuclear Plant => Turning ON');
+  }
+}
 
+class SolarPlant {
+  turnOn() {
+    print('Solar Plant => Turning ON');
+  }
 }
